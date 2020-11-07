abaa8f86ae38b7fddbba681570385dc27e77376e
ElliotGerard123 <73714771+ElliotGerard123@users.noreply.github.com>
2020-11-07 14:16:23 +0100

    Add files via upload

---
+++ __FaceRecognition1/Code/face_train.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+import cv2
+import os
+from PIL import Image
+import numpy as np
+import pickle
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+image_dir = os.path.join(BASE_DIR,"../images")
+#face recognizer LBPH recognizer
+
+
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+current_id = 0
+label_ids = {}
+y_labels = []
+x_train = []
+
+
+for root,dirs,files in os.walk(image_dir):
+    for file in files:
+        if file.endswith("png") or file.endswith("jpg"):
+            path = os.path.join(root, file)
+            label = os.path.basename(os.path.dirname(path)).replace(" ","-").lower()
+            #print(label, path)
+            # We create an ID for one of these labels
+            if not label in label_ids:
+                 
+                label_ids[label] = current_id
+                current_id +=1
+            id_ = label_ids[label]
+            
+           # print(label_ids)
+           # x_train.append(path) # some number
+           # y_labels.append(label) #verify this image, turn it into a numpy array and gray
+            
+            pil_image =Image.open(path).convert("L") # here we convert image into numbers "in array"
+            
+            image_array = np.array(pil_image, "uint8")
+            
+            #print(image_array)
+            faces = face_cascade.detectMultiScale(image_array,scaleFactor= 1.5, minNeighbors = 5) #face detection inside image
+            
+            for(x,y,w,h) in faces:
+                roi=image_array[y:y+h,x:x+w]
+                x_train.append(roi)
+                y_labels.append(id_)
+                
+#print(y_labels)
+#print(x_train)
+
+with open("labels.pickle", 'wb') as f:
+    pickle.dump(label_ids,f)
+    
+recognizer.train(x_train, np.array(y_labels))
+recognizer.save("trainner.yml")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
